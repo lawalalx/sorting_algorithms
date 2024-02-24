714bd39114fbd4871fb76fbaa0cff6efac71c822
lawalalx <lawal.alx@gmail.com>
2024-02-23 22:07:30 -0800

    test

--- sort.h
+++ sort.h
@@ -49,5 +49,4 @@ void quick_sort_hoare(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
 void bitonic_merge(int *array, int l, int r, int direction);
-
 #endif
